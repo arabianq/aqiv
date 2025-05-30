d26a44e6dbdcdef6d9d77df76ed9506043f83bbf
Alexander Tarasov <a.tevg@ya.ru>
2025-05-30 17:07:00 +0300

    std::env -> clap::Parser

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 #![windows_subsystem = "windows"]
-use std::env;
-use std::path::Path;
-
 mod app;
 
+use clap::Parser;
+use std::path::Path;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    file_path: String,
+}
+
 fn main() -> Result<(), eframe::Error> {
-    let args: Vec<String> = env::args().skip(1).collect();
+    let args = Args::parse();
 
-    if args.is_empty() {
-        println!("No file path provided!");
-        std::process::exit(-1);
+    let img_path = Path::new(&args.file_path).to_path_buf();
+    if !img_path.exists() {
+        println!("File {:?} does not exist!", img_path);
+        std::process::exit(1);
     }
 
-    let file_path = Path::new(&args[0]).to_path_buf();
-
-    if !file_path.exists() {
-        println!("File {:?}  does not exist!", file_path);
-        std::process::exit(-1);
-    }
-
-    app::run(file_path)
+    app::run(img_path)
 }
