a765121bfaebd9825e4d753cadda4dd3aff5344a
arabian <a.tevg@ya.ru>
2025-07-08 01:22:52 +0300

    canonicalize img_path

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 mod app;
 
 use clap::Parser;
+use std::fs;
 use std::path::Path;
 
 #[derive(Parser, Debug)]
@@ -13,11 +14,13 @@ struct Args {
 fn main() -> Result<(), eframe::Error> {
     let args = Args::parse();
 
-    let img_path = Path::new(&args.file_path).to_path_buf();
+    let mut img_path = Path::new(&args.file_path).to_path_buf();
     if !img_path.exists() {
         println!("File {:?} does not exist!", img_path);
         std::process::exit(1);
     }
+
+    img_path = fs::canonicalize(&img_path).unwrap_or(img_path);
 
     app::run(img_path)
 }
