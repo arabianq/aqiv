9da780fc9b5bed0abfa572f5a1a82e5739d7d5a8
arabian <a.tevg@ya.ru>
2025-07-11 17:57:47 +0300

    update build scripts

--- build_scripts/build_all.sh
+++ build_scripts/build_all.sh
@@ -5,3 +5,9 @@ cd "$(dirname "$(realpath "$0")")/.." || exit
 bash ./build_scripts/build_deb.sh
 bash ./build_scripts/build_rpm.sh
 bash ./build_scripts/build_windows.sh
+
+mkdir ./target/for_github_release
+cp "$(find ./target/x86_64-unknown-linux-gnu/debian/aqiv_*_amd64.deb | sort -V | tail -n 1)" ./target/for_github_release/
+cp "$(find ./target/generate-rpm/aqiv-*.x86_64.rpm | sort -V | tail -n 1)" ./target/for_github_release/
+cp ./target/x86_64-unknown-linux-gnu/release/aqiv ./target/for_github_release/aqiv-x86_64-linux-gnu
+cp ./target/x86_64-pc-windows-gnu/release/aqiv.exe ./target/for_github_release/aqiv-x86_64-windows-gnu.exe
--- build_scripts/build_deb.sh
+++ build_scripts/build_deb.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$(realpath "$0")")/.." || exit
 
 bash ./build_scripts/build_linux.sh
 cargo install cargo-deb
-cargo deb
+cargo deb --target x86_64-unknown-linux-gnu
