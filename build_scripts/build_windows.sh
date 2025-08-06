a27158d7a8b9c64170f4c08c5dcd27d9c52011f8
arabian <a.tevg@ya.ru>
2025-08-07 02:13:34 +0300

    change panic to "unwind"

--- build_scripts/build_windows.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# Change dir to the cargo project
-cd "$(dirname "$(realpath "$0")")/.." || exit
-
-RUST_TARGET=${1:-"x86_64-pc-windows-gnu"}
-
-rustup target add "$RUST_TARGET"
-cargo build --release --target "$RUST_TARGET"
