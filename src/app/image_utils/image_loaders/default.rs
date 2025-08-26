cc2107ec7315d2b9f3757618dc9ccae219373626
arabian <a.tevg@ya.ru>
2025-08-26 18:37:04 +0300

    remove some garbage

--- src/app/image_utils/image_loaders/default.rs
+++ src/app/image_utils/image_loaders/default.rs
@@ -1,15 +1,13 @@
 use image::DynamicImage;
-use magick_rust::{MagickWand, OrientationType};
+use magick_rust::MagickWand;
 
 use std::error::Error;
 
 pub fn load_image_default(buf: &Vec<u8>) -> Result<DynamicImage, Box<dyn Error>> {
-    let mut wand = MagickWand::new();
+    let wand = MagickWand::new();
     wand.read_image_blob(buf)?;
 
     wand.auto_orient();
-
-    println!("{:?}", wand.get_image_orientation());
 
     let blob_png = wand.write_image_blob("PNG")?;
     let dynamic_image = image::load_from_memory(&blob_png)?;
