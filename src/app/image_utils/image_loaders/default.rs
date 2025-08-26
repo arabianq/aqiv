65d44d68565b30e0ff21c91c0c6cd61b977fdb78
arabian <a.tevg@ya.ru>
2025-08-26 18:30:28 +0300

    use auto_orient() in load_image_default()

--- src/app/image_utils/image_loaders/default.rs
+++ src/app/image_utils/image_loaders/default.rs
@@ -1,11 +1,15 @@
 use image::DynamicImage;
-use magick_rust::MagickWand;
+use magick_rust::{MagickWand, OrientationType};
 
 use std::error::Error;
 
 pub fn load_image_default(buf: &Vec<u8>) -> Result<DynamicImage, Box<dyn Error>> {
-    let wand = MagickWand::new();
+    let mut wand = MagickWand::new();
     wand.read_image_blob(buf)?;
+
+    wand.auto_orient();
+
+    println!("{:?}", wand.get_image_orientation());
 
     let blob_png = wand.write_image_blob("PNG")?;
     let dynamic_image = image::load_from_memory(&blob_png)?;
